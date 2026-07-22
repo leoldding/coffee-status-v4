2b83ae861d2189848cde45c36737cb17df76b844
leoldding <leo.ld.ding@gmail.com>
2026-07-21 21:30:07 -0400

    temp put log

--- backend/internal/handlers/coffee.go
+++ backend/internal/handlers/coffee.go
@@ -70,6 +70,8 @@ func (c *CoffeeHandler) GetStatus(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *CoffeeHandler) PutStatus(w http.ResponseWriter, r *http.Request) {
+    log.Println("PutStatus handler reached")
+
 	defer r.Body.Close()
 
 	ctx := r.Context()
